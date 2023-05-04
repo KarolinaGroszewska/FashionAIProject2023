8e85292611291db75225f1ff9c113a364cd71c69
KarolinaGroszewska <karolina.groszewska@gmail.com>
2023-05-03 21:10:58 -0500

    CNN for fashion_mnist

--- CNN/test_cnn.py
+++ CNN/test_cnn.py
@@ -1,36 +1,111 @@
-# Source: https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-fashion-mnist-clothing-classification/
+from numpy import mean
+from numpy import std
 from matplotlib import pyplot
-# TODO: swap datasets
-import tensorflow as tf
-fashion_mnist = tf.keras.datasets.fashion_mnist
+from sklearn.model_selection import KFold
+from keras.datasets import fashion_mnist
+from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.optimizers import SGD
-(trainX, trainy), (testX, testy) = fashion_mnist.load_data()
-print('Train: X=%s, y=%s' % (trainX.shape, trainy.shape))
-print('Test: X=%s, y=%s' % (testX.shape, testy.shape))
-# plot first few images
-for i in range(9):
-	# define subplot
-	pyplot.subplot(330 + 1 + i)
-	# plot raw pixel data
-	pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
-# show the figure
-pyplot.show()
+import os, glob
 
+    # The classes are:
+
+    # | Label | Description |
+    # |:-----:|-------------|
+    # |   0   | Top |
+    # |   1   | Bottom |
+    # |   2   | Full-body |
+    # |   3   | Shoe |
+
+
+# def create_dataset():
+    # converts our segment anything .npzs and the CSV of values into a tuple of numpy arrays
+    # which are used in the CNN to train the dataset
+    # folder_path = '../images'
+    # for root, dirs, files in os.walk(folder_path, topdown=False):
+    #     for name in files:
+    #         print(os.path.join(root, name))
+    #         stuff
+    #     for name in dirs:
+    #         print(os.path.join(root, name))
+    #         stuff
+
+
+
+
+# load train and test dataset
+def load_dataset():
+    # TODO: change fashion_mnist.load_data() to a different set
+    (trainX, trainY), (testX, testY) = fashion_mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+# scale pixels
+# def prep_pixels(train, test):
+#     # convert from integers to floats
+#     train_norm = train.astype('float32')
+#     test_norm = test.astype('float32')
+#     # normalize to range 0-1
+#     train_norm = train_norm / 255.0
+#     test_norm = test_norm / 255.0
+#     # return normalized images
+#     return train_norm, test_norm
+    
 # define cnn model
 def define_model():
- model = Sequential()
- model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
- model.add(MaxPooling2D((2, 2)))
- model.add(Flatten())
- model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
- model.add(Dense(10, activation='softmax'))
- # compile model
- opt = SGD(lr=0.01, momentum=0.9)
- model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
- return model
- 
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# evaluate a model using k-fold cross-validation
+def evaluate_model(dataX, dataY, n_folds=5):
+    scores, histories = list(), list()
+    # prepare cross validation
+    kfold = KFold(n_folds, shuffle=True, random_state=1)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(dataX):
+        # define model
+        model = define_model()
+        # select rows for train and test
+        trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
+        # fit model
+        history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
+        # evaluate model
+        _, acc = model.evaluate(testX, testY, verbose=0)
+        print('> %.3f' % (acc * 100.0))
+        # append scores
+        scores.append(acc)
+        histories.append(history)
+    return scores
+
+# summarize model performance
+def summarize_performance(scores):
+	# print summary
+	print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+        
+ # run the test harness for evaluating a model
+def run_test_harness():
+ # load dataset
+ trainX, trainY, testX, testY = load_dataset()
+ # evaluate model
+ scores = evaluate_model(trainX, trainY)
+ # learning curves
+ summarize_performance(scores)
+
+run_test_harness()
